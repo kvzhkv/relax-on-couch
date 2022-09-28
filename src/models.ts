79ffe0687f26d1b680e4c995c3653988ab99820e
Nickolai-dev <n.solenii2016@gmail.com>
2022-09-28 14:16:08 +0700

    add analyzer types;

--- src/models.ts
+++ src/models.ts
@@ -78,4 +78,49 @@ declare namespace RelaxOnCouch {
         rev: string;
         ok: boolean;
     }
+
+    type LuceneAnalyzer =
+        | "classic"
+        | "email"
+        | "keyword"
+        | "simple"
+        | "standard"
+        | "whitespace"
+        | LangSpecAnalyzer;
+
+    type LangSpecAnalyzer =
+        | "arabic" // org.apache.lucene.analysis.ar.ArabicAnalyzer
+        | "armenian" // org.apache.lucene.analysis.hy.ArmenianAnalyzer
+        | "basque" // org.apache.lucene.analysis.eu.BasqueAnalyzer
+        | "bulgarian" // org.apache.lucene.analysis.bg.BulgarianAnalyzer
+        | "brazilian" // org.apache.lucene.analysis.br.BrazilianAnalyzer
+        | "catalan" // org.apache.lucene.analysis.ca.CatalanAnalyzer
+        | "cjk" // org.apache.lucene.analysis.cjk.CJKAnalyzer (Chinese/Japanese/Korean)
+        | "chinese" // org.apache.lucene.analysis.cn.smart.SmartChineseAnalyzer
+        | "czech" // org.apache.lucene.analysis.cz.CzechAnalyzer
+        | "danish" // org.apache.lucene.analysis.da.DanishAnalyzer
+        | "dutch" // org.apache.lucene.analysis.nl.DutchAnalyzer
+        | "english" // org.apache.lucene.analysis.en.EnglishAnalyzer
+        | "finnish" // org.apache.lucene.analysis.fi.FinnishAnalyzer
+        | "french" // org.apache.lucene.analysis.fr.FrenchAnalyzer
+        | "german" // org.apache.lucene.analysis.de.GermanAnalyzer
+        | "greek" // org.apache.lucene.analysis.el.GreekAnalyzer
+        | "galician" // org.apache.lucene.analysis.gl.GalicianAnalyzer
+        | "hindi" // org.apache.lucene.analysis.hi.HindiAnalyzer
+        | "hungarian" // org.apache.lucene.analysis.hu.HungarianAnalyzer
+        | "indonesian" // org.apache.lucene.analysis.id.IndonesianAnalyzer
+        | "irish" // org.apache.lucene.analysis.ga.IrishAnalyzer
+        | "italian" // org.apache.lucene.analysis.it.ItalianAnalyzer
+        | "japanese" // org.apache.lucene.analysis.ja.JapaneseAnalyzer
+        | "latvian" // org.apache.lucene.analysis.lv.LatvianAnalyzer
+        | "norwegian" // org.apache.lucene.analysis.no.NorwegianAnalyzer
+        | "persian" // org.apache.lucene.analysis.fa.PersianAnalyzer
+        | "polish" // org.apache.lucene.analysis.pl.PolishAnalyzer
+        | "portuguese" // org.apache.lucene.analysis.pt.PortugueseAnalyzer
+        | "romanian" // org.apache.lucene.analysis.ro.RomanianAnalyzer
+        | "russian" // org.apache.lucene.analysis.ru.RussianAnalyzer
+        | "spanish" // org.apache.lucene.analysis.es.SpanishAnalyzer
+        | "swedish" // org.apache.lucene.analysis.sv.SwedishAnalyzer
+        | "thai" // org.apache.lucene.analysis.th.ThaiAnalyzer
+        | "turkish"; // org.apache.lucene.analysis.tr.TurkishAnalyzer
 }
