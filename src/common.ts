c752bca1b0e049604cab81e351c983f07429ccf1
Nickolai-dev <n.solenii2016@gmail.com>
2022-11-24 21:45:19 +0700

    Revert "fix bug in `request`;"
    This reverts commit b1b342bc769e3b1dad7af2d52062fd98dbcf50cb.

--- src/common.ts
+++ src/common.ts
@@ -48,7 +48,7 @@ export abstract class RelaxOnCouchBase {
             controller.abort();
         }, this.timeout);
         try {
-            const res = await fetch(`http://${this.baseUrl}/${path}`, {
+            const res = await fetch(`${this.baseUrl}${path}`, {
                 method,
                 headers: {
                     "Content-Type": "application/json",
