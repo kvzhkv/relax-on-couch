6a65550c7cb7c7a447a3113173ed299e1394fee4
Nickolai-dev <n.solenii2016@gmail.com>
2023-05-22 08:37:56 +0400

    extend request method;

--- src/common.ts
+++ src/common.ts
@@ -1,4 +1,4 @@
-import { ServerConfig } from "./models.js";
+import { AbortControlObject, ServerConfig } from "./models.js";
 import fetch from "node-fetch";
 import http from "http";
 import https from "https";
@@ -26,60 +26,91 @@ export abstract class RelaxOnCouchBase {
         });
     }
 
-    protected async request<T>(
+    protected request<T>(
         path: string,
         method: string,
         params?: object,
-    ): Promise<T> {
+    ): Promise<T>;
+    protected request<T>(
+        path: string,
+        method: string,
+        params: object | undefined,
+        giveControl: true,
+    ): [Promise<T>, AbortControlObject];
+    protected request<T>(
+        path: string,
+        method: string,
+        params?: object,
+        giveAbortControl?: boolean,
+    ): [Promise<T>, AbortControlObject] | Promise<T> {
         const controller = new AbortController();
-        const timeout = setTimeout(() => {
-            controller.abort();
-        }, this.timeout);
-        try {
-            const res = await fetch(`${this.baseUrl}${path}`, {
-                method,
-                headers: {
-                    "Content-Type": "application/json",
-                    Authorization: this.auth,
-                },
-                body: params ? JSON.stringify(params) : undefined,
-                signal: controller.signal,
-                agent: this.agent,
+        const timeout =
+            !giveAbortControl &&
+            setTimeout(() => {
+                controller.abort();
+            }, this.timeout);
+        const promise: Promise<T> = fetch(`${this.baseUrl}${path}`, {
+            method,
+            headers: {
+                "Content-Type": "application/json",
+                Authorization: this.auth,
+            },
+            body: params ? JSON.stringify(params) : undefined,
+            signal: controller.signal,
+            agent: this.agent,
+        })
+            .then(res => {
+                const contentType = res.headers.get("Content-Type");
+
+                if (!contentType || !contentType.includes("application/json")) {
+                    res.text().then(text => {
+                        console.error(text);
+                        throw new Error(
+                            `RelaxOnCouch: upsupported Content-Type, expected application/json, recieved ${
+                                contentType || "null"
+                            }`,
+                        );
+                    });
+                }
+
+                return res.json().then((json: any) => {
+                    if (json.error) {
+                        const error = new Error(`RelaxOnCouch: ${json.error}`);
+                        (error as any).status = res.status;
+                        (error as any).reason = json.reason;
+                        throw error;
+                    }
+
+                    return json;
+                });
+            })
+            .catch(e => {
+                if (!e.message) {
+                    e.message = "RelaxOnCouch: Unknown error";
+                }
+                if (e.message.indexOf("RelaxOnCouch") === -1) {
+                    e.message = `RelaxOnCouch: ${e.message}`;
+                }
+                console.error(e);
+                throw e;
+            })
+            .finally(() => {
+                timeout && clearTimeout(timeout);
             });
-
-            const contentType = res.headers.get("Content-Type");
-
-            if (!contentType || !contentType.includes("application/json")) {
-                const text = await res.text();
-                console.error(text);
-                throw new Error(
-                    `RelaxOnCouch: upsupported Content-Type, expected application/json, recieved ${
-                        contentType || "null"
-                    }`,
-                );
-            }
-
-            const json: any = await res.json();
-
-            if (json.error) {
-                const error = new Error(`RelaxOnCouch: ${json.error}`);
-                (error as any).status = res.status;
-                (error as any).reason = json.reason;
-                throw error;
-            }
-
-            return json;
-        } catch (e: any) {
-            if (!e.message) {
-                e.message = "RelaxOnCouch: Unknown error";
-            }
-            if (e.message.indexOf("RelaxOnCouch") === -1) {
-                e.message = `RelaxOnCouch: ${e.message}`;
-            }
-            console.error(e);
-            throw e;
-        } finally {
-            clearTimeout(timeout);
-        }
+        return giveAbortControl
+            ? [
+                  promise,
+                  {
+                      abort: () => controller.abort(),
+                      onAbort: new Promise(resolve => {
+                          promise.catch(e => {
+                              if (e.name === "AbortError") {
+                                  resolve();
+                              }
+                          });
+                      }),
+                  },
+              ]
+            : promise;
     }
 }
