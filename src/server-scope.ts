4e1ddd714865299fb9552af995f0911083412d62
Konstantin Zhukov <kvzhkv@gmail.com>
2024-10-26 11:04:38 +0200

    add params to createDb method

--- src/server-scope.ts
+++ src/server-scope.ts
@@ -14,8 +14,30 @@ export class RelaxOnCouch extends RelaxOnCouchBase {
         this.serverConfig = config;
     }
 
-    public async createDb(dbName: string) {
-        return await this.request(dbName, "PUT");
+    private isPositiveInt(v: number): boolean {
+        return Number.isInteger(v) && v > 0;
+    }
+
+    public async createDb(
+        dbName: string,
+        {
+            n,
+            q,
+            partitioned,
+        }: { q?: number; n?: number; partitioned?: boolean } = {
+            partitioned: false,
+        },
+    ) {
+        if (n !== undefined && !this.isPositiveInt(n)) {
+            throw new Error("n must be a positive integer");
+        }
+        if (q !== undefined && !this.isPositiveInt(q)) {
+            throw new Error("q must be a positive integer");
+        }
+        const queryParams: string[] = [`partitioned=${partitioned}`];
+        if (n) queryParams.push(`n=${n}`);
+        if (q) queryParams.push(`q=${q}`);
+        return await this.request(`${dbName}?${queryParams.join("&")}`, "PUT");
     }
 
     public async deleteDb(dbName: string) {
