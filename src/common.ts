bf7daf26b862689b1a440c7e94e08c410ccc524e
Konstantin Zhukov <kvzhkv@gmail.com>
2025-10-22 15:49:09 +0200

    remove agent from common.ts

--- src/common.ts
+++ src/common.ts
@@ -1,13 +1,9 @@
 import { ServerConfig } from "./models.js";
-import fetch from "node-fetch";
-import http from "http";
-import https from "https";
 
 export abstract class RelaxOnCouchBase {
     readonly baseUrl: string;
     private timeout: number;
     private auth: string;
-    private agent: http.Agent | https.Agent;
 
     constructor({
         url,
@@ -19,11 +15,6 @@ export abstract class RelaxOnCouchBase {
         this.auth = `Basic ${Buffer.from(`${username}:${password}`).toString(
             "base64",
         )}`;
-        this.agent = new (url.startsWith("https:") ? https : http).Agent({
-            keepAlive: true,
-            maxSockets: 50,
-            keepAliveMsecs: 30000,
-        });
     }
 
     protected async request<T>(
@@ -45,7 +36,6 @@ export abstract class RelaxOnCouchBase {
                 },
                 body: params ? JSON.stringify(params) : undefined,
                 signal: controller.signal,
-                agent: this.agent,
             });
 
             const contentType = res.headers.get("Content-Type");
